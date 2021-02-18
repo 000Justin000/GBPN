4b65fb12a00333a1fe8eefc1f148bbb826c2730a
Junteng Jia <juntengjia@hotmail.com>
2021-02-18 12:13:11 -0500

    update

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ class BPConv(MessagePassing):
     def __init__(self, n_channels, learn_coupling=False):
         super(BPConv, self).__init__(aggr='add')
         self.learn_coupling = learn_coupling
-        self.weights = nn.Parameter(torch.zeros(n_channels*(n_channels-1)//2))
+        self.weights = nn.Parameter(torch.zeros(n_channels*(n_channels+1)//2))
         self.n_channels = n_channels
 
     def get_logH(self):
         logT = torch.zeros(self.n_channels, self.n_channels).to(self.weights.device)
-        rid, cid = torch.tril_indices(self.n_channels, self.n_channels, -1)
-        logT[rid, cid] = F.logsigmoid((self.weights - self.weights.mean()))
-        logH = (logT + logT.transpose(0,1))
+        rid, cid = torch.tril_indices(self.n_channels, self.n_channels)
+        logT[rid, cid] = F.logsigmoid(self.weights)
+        logH = (logT + logT.transpose(0,1).triu(1))
         return (logH if self.learn_coupling else logH.detach())
 
     def forward(self, x, edge_index, info):
@@ -75,6 +75,15 @@ class BPGNN(nn.Module):
         return log_b
 
 
+def cts_coupling(edge_index, y):
+    idx, cts = torch.stack((y[edge_index[0]], y[edge_index[1]]), dim=0).unique(dim=1, return_counts=True)
+    C = torch.zeros(y.max()+1, y.max()+1, device=y.device)
+    C[idx[0], idx[1]] = cts.float()
+    d = C.sum(dim=0)
+    coupling = (d**-0.5).view(-1,1) * C * (d**-0.5).view(1,-1)
+    return coupling
+
+
 def run(dataset, split, model, num_hidden, device, learning_rate, develop):
 
     if dataset == 'Cora':
@@ -108,7 +117,7 @@ def run(dataset, split, model, num_hidden, device, learning_rate, develop):
     elif model == 'GAT':
         gnn = GAT(num_features, num_classes, 32, 0.6)
     elif model == 'BPGNN':
-        gnn = BPGNN(num_features, num_classes, 128, num_hidden, 0.3, False)
+        gnn = BPGNN(num_features, num_classes, 128, num_hidden, 0.3, True)
     else:
         raise Exception('unexpected model')
     gnn = gnn.to(device)
@@ -116,6 +125,7 @@ def run(dataset, split, model, num_hidden, device, learning_rate, develop):
     optimizer = torch.optim.AdamW([{'params': gnn.parameters(), 'lr': learning_rate}], weight_decay=2.5e-4)
 
     def train():
+        # print(gnn.conv.get_logH().exp())
         gnn.train()
         optimizer.zero_grad()
         b = gnn(x, edge_index, rv=rv)
@@ -141,15 +151,14 @@ def run(dataset, split, model, num_hidden, device, learning_rate, develop):
             b = gnn(x, edge_index, rv=rv)
             if develop:
                 print('evaluation, train accuracy: {:5.3f}, val accuracy: {:5.3f}, test accuracy: {:5.3f}'.format(acc(b, y, train_mask), acc(b, y, val_mask), acc(b, y, test_mask)), flush=True)
-        return acc(b, y, test_mask)
+        return acc(b, y, val_mask), acc(b, y, test_mask)
 
-    opt_val = 0.0
-    opt_test = 0.0
+    best_val, opt_val, opt_test = 0.0, 0.0, 0.0
     for epoch in range(300):
         val = train()
-        if opt_val < val:
-            opt_val = val
-            opt_test = evaluation()
+        if best_val < val:
+            best_val = val
+            opt_val, opt_test = evaluation()
 
     if develop:
         print('optimal val accuracy: {:7.5f}, optimal test accuracy: {:7.5f}'.format(opt_val, opt_test))
