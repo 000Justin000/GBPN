60d9a907e103a2a2e9477045f00daacb3e93a4c6
Junteng Jia <jj585@cornell.edu>
2021-03-01 03:15:36 -0500

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-# make run_Animals
-# make run_SGC
-# make run_GCN
-# make run_SAGE
-# make run_GAT
-# make run_MLP
+make run_SGC
+make run_GCN
+make run_SAGE
+make run_GAT
+make run_MLP
 make run_BPGNN_FFF
 make run_BPGNN_TFF
 make run_BPGNN_TTF
 make run_BPGNN_TTT
+make run_Animals
