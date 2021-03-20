ccfa52014bdf75f501f710374787db85b14b4633
Junteng Jia <juntengjia@hotmail.com>
2021-03-20 12:33:16 -0400

    update

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from torch_geometric.nn import GCNConv
 from torch_geometric.transforms import ToSparseTensor
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import degree, is_undirected, subgraph
+from sklearn.metrics import roc_auc_score
 from utils import *
 import matplotlib
 import matplotlib.pyplot as plt
@@ -31,33 +32,81 @@ def get_cts(edge_index, y):
     return ctsm, sqrt_deg_inv.view(-1, 1) * ctsm * sqrt_deg_inv.view(1, -1)
 
 
+def classification_accuracy(log_b, y):
+    if type(log_b) == torch.Tensor:
+        log_b = log_b.detach().cpu().numpy()
+    if type(y) == torch.Tensor:
+        y = y.detach().cpu().numpy()
+    return (log_b.argmax(-1) == y).sum() / y.shape[0]
+
+
+def roc_auc(log_b, y):
+    if type(log_b) == torch.Tensor:
+        log_b = log_b.detach().cpu().numpy()
+    if type(y) == torch.Tensor:
+        y = y.detach().cpu().numpy()
+    return roc_auc_score(y, np.exp(log_b[:, 1]))
+
+
 def run(dataset, homo_ratio, split, model_name, num_hidden, device, learning_rate, weighted_BP, learn_H, eval_C, verbose):
     if dataset == 'Cora':
         data = load_citation('Cora', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'CiteSeer':
         data = load_citation('CiteSeer', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'PubMed':
         data = load_citation('PubMed', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Coauthor_CS':
         data = load_coauthor('CS', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Coauthor_Physics':
         data = load_coauthor('Physics', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'County_Facebook':
         data = load_county_facebook(split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Sex':
         data = load_sexual_interaction(split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Animal2':
         data = load_animal2(homo_ratio=homo_ratio, split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Animal3':
         data = load_animal3(homo_ratio=homo_ratio, split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Squirrel':
         data = load_wikipedia('Squirrel', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'Chameleon':
         data = load_wikipedia('Chameleon', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'OGBN_arXiv':
         data = load_ogbn('arxiv', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
     elif dataset == 'OGBN_Products':
         data = load_ogbn('products', split=split)
+        c_weight = None
+        accuracy_fun = classification_accuracy
+    elif dataset == 'JPMC_Fraud_Detection':
+        x, y, info = load_jpmc_fraud()
+        data = preprocess_gnn_jpmc_fraud(x, y, info, split=split)
+        _, cts = data.y.unique(return_counts=True)
+        c_weight = (cts**-1.0) / (cts**-1.0).sum()
+        accuracy_fun = roc_auc
     else:
         raise Exception('unexpected dataset')
 
@@ -66,6 +115,7 @@ def run(dataset, homo_ratio, split, model_name, num_hidden, device, learning_rat
     num_nodes, num_features = x.shape
     num_classes = len(torch.unique(y))
     train_mask, val_mask, test_mask = data.train_mask, data.val_mask, data.test_mask
+    c_weight = None if (c_weight is None) else c_weight.to(device)
 
     if (model_name == 'GBPN') and weighted_BP and (edge_weight is None):
         deg = degree(edge_index[1], num_nodes)
@@ -82,7 +132,7 @@ def run(dataset, homo_ratio, split, model_name, num_hidden, device, learning_rat
             num_hops = 5
         num_samples = -1
         num_epoches = 200
-    elif dataset in ['OGBN_arXiv', 'OGBN_Products']:
+    elif dataset in ['OGBN_arXiv', 'OGBN_Products', 'JPMC_Fraud_Detection']:
         subgraph_sampler = CSubtreeSampler(num_nodes, x, y, edge_index, edge_weight)
         max_batch_size = min(math.ceil(train_mask.sum()/10.0), 5120)
         if model_name == 'MLP':
@@ -113,70 +163,72 @@ def run(dataset, homo_ratio, split, model_name, num_hidden, device, learning_rat
 
     def train(num_hops=2, num_samples=5):
         model.train()
-        n_batch = total_loss = total_correct = 0.0
+        total_loss = 0.0
+        log_b_list, gth_y_list = [], []
         for batch_size, batch_nodes, batch_x, batch_y, batch_deg0, \
             subgraph_size, subgraph_nodes, subgraph_x, subgraph_y, subgraph_deg0, \
             subgraph_edge_index, subgraph_edge_weight, subgraph_rv in subgraph_sampler.get_generator(train_mask, max_batch_size, num_hops, num_samples, device):
             optimizer.zero_grad()
             subgraph_log_b = model(subgraph_x, subgraph_edge_index, edge_weight=subgraph_edge_weight, agg_scaling=get_scaling(subgraph_deg0, degree(subgraph_edge_index[1], subgraph_size)), rv=subgraph_rv, K=num_hops)
-            loss = F.nll_loss(subgraph_log_b[:batch_size], batch_y)
+            loss = F.nll_loss(subgraph_log_b[:batch_size], batch_y, weight=c_weight)
             loss.backward()
             optimizer.step()
-            n_batch += 1
-            total_loss += float(loss)
-            total_correct += (subgraph_log_b[:batch_size].argmax(-1) == batch_y).sum().item()
-
+            total_loss += float(loss)*batch_size
+            log_b_list.append(subgraph_log_b[:batch_size].detach())
+            gth_y_list.append(batch_y)
+        mean_loss = total_loss / int(train_mask.sum())
+        accuracy = accuracy_fun(torch.cat(log_b_list, dim=0), torch.cat(gth_y_list, dim=0))
         if verbose:
-            print('step {:5d}, train loss: {:5.3f}, train accuracy: {:5.3f}'.format(epoch, total_loss/n_batch, total_correct/train_mask.sum().item()), end='    ')
-
-        return total_correct/train_mask.sum().item()
+            print('step {:5d}, train loss: {:5.3f}, train accuracy: {:5.3f}'.format(epoch, mean_loss, accuracy), end='    ')
+        return accuracy
 
     def evaluation(mask, num_hops=2, num_samples=5, partition='train'):
         model.eval()
-
-        total_correct = 0.0
-        with torch.no_grad():
+        log_b_list, gth_y_list = [], []
+        for batch_size, batch_nodes, batch_x, batch_y, batch_deg0, \
+            subgraph_size, subgraph_nodes, subgraph_x, subgraph_y, subgraph_deg0, \
+            subgraph_edge_index, subgraph_edge_weight, subgraph_rv in subgraph_sampler.get_generator(mask, max_batch_size, num_hops, num_samples, device):
+            subgraph_log_b = model(subgraph_x, subgraph_edge_index, edge_weight=subgraph_edge_weight, agg_scaling=get_scaling(subgraph_deg0, degree(subgraph_edge_index[1], subgraph_size)), rv=subgraph_rv, K=num_hops)
+            log_b_list.append(subgraph_log_b[:batch_size].detach())
+            gth_y_list.append(batch_y)
+        accuracy = accuracy_fun(torch.cat(log_b_list, dim=0), torch.cat(gth_y_list, dim=0))
+        if verbose:
+            print('{:>5s} inductive accuracy: {:5.3f}'.format(partition, accuracy), end='    ')
+        if type(model) == GBPN and eval_C:
+            sum_conv = SumConv()
+            log_b_list, gth_y_list = [], []
             for batch_size, batch_nodes, batch_x, batch_y, batch_deg0, \
                 subgraph_size, subgraph_nodes, subgraph_x, subgraph_y, subgraph_deg0, \
                 subgraph_edge_index, subgraph_edge_weight, subgraph_rv in subgraph_sampler.get_generator(mask, max_batch_size, num_hops, num_samples, device):
-                subgraph_log_b = model(subgraph_x, subgraph_edge_index, edge_weight=subgraph_edge_weight, agg_scaling=get_scaling(subgraph_deg0, degree(subgraph_edge_index[1], subgraph_size)), rv=subgraph_rv, K=num_hops)
-                total_correct += (subgraph_log_b[:batch_size].argmax(-1) == batch_y).sum().item()
-        if verbose:
-            print('{:>5s} inductive accuracy: {:5.3f}'.format(partition, total_correct/mask.sum().item()), end='    ')
-
-        if type(model) == GBPN and eval_C:
-            sum_conv = SumConv()
-            total_correct = 0.0
-            with torch.no_grad():
-                for batch_size, batch_nodes, batch_x, batch_y, batch_deg0, \
-                    subgraph_size, subgraph_nodes, subgraph_x, subgraph_y, subgraph_deg0, \
-                    subgraph_edge_index, subgraph_edge_weight, subgraph_rv in subgraph_sampler.get_generator(mask, max_batch_size, num_hops, num_samples, device):
-                    subgraphC_mask = train_mask[subgraph_nodes]
-                    subgraphR_mask = torch.logical_not(subgraphC_mask)
-                    log_c = torch.zeros(subgraph_size, num_classes).to(device)
-                    log_c[subgraphC_mask] = model.bp_conv.get_logH()[subgraph_y[subgraphC_mask]]
-                    subgraphR_phi = sum_conv(log_c, subgraph_edge_index, subgraph_edge_weight)[subgraphR_mask]
-                    subgraphR_edge_index, subgraphR_edge_weight = subgraph(subgraphR_mask, subgraph_edge_index, subgraph_edge_weight, relabel_nodes=True)
-                    subgraphR_edge_index, subgraphR_edge_weight, subgraphR_rv = process_edge_index(subgraphR_mask.sum().item(), subgraphR_edge_index, subgraphR_edge_weight)
-                    subgraph_log_b = torch.zeros(subgraph_size, num_classes).to(device)
-                    subgraph_log_b[subgraphC_mask] = F.one_hot(subgraph_y[subgraphC_mask], num_classes).float().to(device)
-                    subgraph_log_b[subgraphR_mask] = model(subgraph_x[subgraphR_mask], subgraphR_edge_index, subgraphR_edge_weight,
-                                                           agg_scaling=get_scaling(subgraph_deg0, degree(subgraph_edge_index[1], subgraph_size))[subgraphR_mask], rv=subgraphR_rv, phi=subgraphR_phi, K=num_hops)
-                    total_correct += (subgraph_log_b[:batch_size].argmax(-1) == batch_y).sum().item()
+                subgraphC_mask = train_mask[subgraph_nodes]
+                subgraphR_mask = torch.logical_not(subgraphC_mask)
+                log_c = torch.zeros(subgraph_size, num_classes).to(device)
+                log_c[subgraphC_mask] = model.bp_conv.get_logH()[subgraph_y[subgraphC_mask]]
+                subgraphR_phi = sum_conv(log_c, subgraph_edge_index, subgraph_edge_weight)[subgraphR_mask]
+                subgraphR_edge_index, subgraphR_edge_weight = subgraph(subgraphR_mask, subgraph_edge_index, subgraph_edge_weight, relabel_nodes=True)
+                subgraphR_edge_index, subgraphR_edge_weight, subgraphR_rv = process_edge_index(subgraphR_mask.sum().item(), subgraphR_edge_index, subgraphR_edge_weight)
+                subgraph_log_b = torch.zeros(subgraph_size, num_classes).to(device)
+                subgraph_log_b[subgraphC_mask] = F.one_hot(subgraph_y[subgraphC_mask], num_classes).float().to(device)
+                subgraph_log_b[subgraphR_mask] = model(subgraph_x[subgraphR_mask], subgraphR_edge_index, subgraphR_edge_weight,
+                                                       agg_scaling=get_scaling(subgraph_deg0, degree(subgraph_edge_index[1], subgraph_size))[subgraphR_mask], rv=subgraphR_rv, phi=subgraphR_phi, K=num_hops)
+                log_b_list.append(subgraph_log_b[:batch_size].detach())
+                gth_y_list.append(batch_y)
+            accuracy = accuracy_fun(torch.cat(log_b_list, dim=0), torch.cat(gth_y_list, dim=0))
             if verbose:
-                print('{:>5s} transductive accuracy: {:5.3f}'.format(partition, total_correct/mask.sum().item()), end='    ')
-
-        return total_correct/mask.sum().item()
+                print('{:>5s} transductive accuracy: {:5.3f}'.format(partition, accuracy), end='    ')
+        return accuracy
 
     max_num_hops = num_hops
     best_val, opt_val, opt_test = 0.0, 0.0, 0.0
     for epoch in range(num_epoches):
         num_hops = 0 if (model_name == 'GBPN' and epoch < 0.05*num_epoches) else max_num_hops
         train(num_hops=num_hops, num_samples=num_samples)
-        val = evaluation(val_mask, num_hops=num_hops, num_samples=num_samples, partition='val')
-        if val > opt_val:
-            opt_val = val
-            opt_test = evaluation(test_mask, num_hops=num_hops, num_samples=num_samples, partition='test')
+
+        with torch.no_grad():
+            val = evaluation(val_mask, num_hops=num_hops, num_samples=num_samples, partition='val')
+            if val > opt_val:
+                opt_val = val
+                opt_test = evaluation(test_mask, num_hops=num_hops, num_samples=num_samples, partition='test')
         print(flush=True)
 
         if type(model) == GBPN and learn_H and verbose:
