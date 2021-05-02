752696d9d8ed41b000261d879b397067501b7bfd
Junteng Jia <jj585@cornell.edu>
2021-05-02 18:14:56 -0400

    update

--- utils.py
+++ utils.py
@@ -438,23 +438,6 @@ class SubtreeSampler:
         return generator()
 
 
-def logsumexp(value, dim=None, keepdim=False):
-    """Numerically stable implementation of the operation
-
-    value.exp().sum(dim, keepdim).log()
-    """
-    if dim is not None:
-        m, _ = torch.max(value, dim=dim, keepdim=True)
-        value0 = value - m
-        if keepdim is False:
-            m = m.squeeze(dim)
-        return m + torch.log(torch.sum(torch.exp(value0), dim=dim, keepdim=keepdim))
-    else:
-        m = torch.max(value)
-        sum_exp = torch.sum(torch.exp(value - m))
-        return m + torch.log(sum_exp)
-
-
 def rand_split(x, ps):
     assert abs(sum(ps) - 1) < 1.0e-10
 
