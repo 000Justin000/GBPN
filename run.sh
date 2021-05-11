a16ae7eafc46adc1068a7c107b10021c3f3b9a2f
Junteng Jia <jj585@cornell.edu>
2021-05-11 14:25:51 -0400

    udpate

--- run.sh
+++ run.sh
@@ -16,9 +16,11 @@
 # make device="cuda:1" run_arXiv
 # make device="cuda:1" run_Products
 
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  10 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  20 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  50 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples 100 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
-  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  -1 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  10 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  20 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  50 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples 100 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  -1 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --device cuda:2 --learn_H --verbose
+
+  python main.py --dataset OGBN_arXiv    --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --learning_rate 1.0e-3 --num_epoches 100 --num_trials 10 --device cuda:1 --loss_option 0 --learn_H --verbose
