e8df2ab7b0c19f9436af92f2e535017f267b80af
Cenk Baykal <baykal@mit.edu>
2021-05-24 21:30:17 -0400

    added --imp_sampling option

--- run.sh
+++ run.sh
@@ -36,10 +36,20 @@
 # python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  10 --initskip_BP 0.00 --lossfunc_BP 5 --deg_scaling --learn_H --eval_C --verbose #--develop
 
 
-python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  1 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
+# Uniform sampling
+python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   1 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
 python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   3 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose 
 python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
 python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  10 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
-python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  20 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
-python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  50 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
-python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples 100 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
+
+# Importance sampling
+python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   1 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose --imp_sampling
+python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   3 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose --imp_sampling
+python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples   5 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose --imp_sampling
+python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  10 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  5 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose --imp_sampling
+
+
+
+# python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  20 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples  50 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
+# python main.py --dataset OGBN_arXiv --model_name GBPN --dim_hidden 256 --num_layers 2 --num_hops 1 --num_samples 100 --dropout_p 0.1 --device cuda --learning_rate 1.0e-3 --num_epoches 100 --num_trials  3 --initskip_BP 0.00 --lossfunc_BP 0 --learn_H --verbose
